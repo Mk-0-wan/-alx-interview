597eeac7ccc24a3b6aa456c2713b983976de9662
gh057 <victormurithi2020@gmail.com>
2024-07-29 11:55:27 +0300

    update the island file

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Making a perimeter island finder"""
+
+
+def find_island_perimeter(grid) -> int:
+    # find the height and with of the grid
+    # height will be same as the graph y axis
+    height = len(grid)
+    # width same as the x axis
+    width = len(grid[0])
+    no_of_islands = 0
+    found_one = 0
+
+    for y in range(height):
+        for x in range(width):
+            if (grid[y][x] == 1):
+                no_of_islands += 1
+                print(f"Position of each island is grid[y({y}), x({x})]")
+                # check up
+                if (y - 1) >= 1 and grid[y - 1][x] == 1:
+                    found_one += 1
+                # check down
+                if (y + 1) < height and grid[y + 1][x] == 1:
+                    found_one += 1
+                # check left
+                if (x - 1) >= 0 and grid[y][x - 1] == 1:
+                    found_one += 1
+                # check right
+                if (x + 1) < width and grid[y][x + 1] == 1:
+                    found_one += 1
+    perimeter = no_of_islands * 4
+    return (perimeter - found_one)    # found_one what is it?
