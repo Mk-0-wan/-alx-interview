3d678f0a8a6ea3b80f8096c4f871518230b45bab
gh057 <victormurithi2020@gmail.com>
2024-08-02 13:57:27 +0300

    man this is so wrong

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
 """Making a perimeter island finder"""
 
-
 def island_perimeter(grid) -> int:
-    """Calculate the area of island in the given grid"""
-    # find the height and with of the grid
-    # height will be same as the graph y axis
+    """Calculate the perimeter of the island in the given grid"""
     height = len(grid)
-    # width same as the x axis
     width = len(grid[0])
-    no_of_islands = 0
-    found_one = 0
+    perimeter = 0
 
     for y in range(height):
         for x in range(width):
-            if (grid[y][x] == 1):
-                no_of_islands += 1
-                # check up
-                if (y - 1) >= 1 and grid[y - 1][x] == 1:
-                    found_one += 1
-                # check down
-                if (y + 1) < height and grid[y + 1][x] == 1:
-                    found_one += 1
-                # check left
-                if (x - 1) >= 0 and grid[y][x - 1] == 1:
-                    found_one += 1
-                # check right
-                if (x + 1) < width and grid[y][x + 1] == 1:
-                    found_one += 1
-    perimeter = no_of_islands * 4
-    return (perimeter - found_one)    # found_one what is it?
+            if grid[y][x] == 1:
+                # Start with 4 for each land cell
+                perimeter += 4
+                # Check up
+                if y > 0 and grid[y - 1][x] == 1:
+                    perimeter -= 2
+                # Check left
+                if x > 0 and grid[y][x - 1] == 1:
+                    perimeter -= 2
+    
+    return perimeter
